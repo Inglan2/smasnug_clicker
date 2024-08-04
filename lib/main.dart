2adae733bc4bed835a8e37331b3679ad58576a1e
Ingo (School Account) <0358381@schoolsnet.act.edu.au>
2024-08-05 07:53:38 +1000

    Seperate into different files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'widgets.dart';
 
 void main() {
   runApp(MainApp());
@@ -88,63 +89,6 @@ class Left extends StatelessWidget {
               )),
         ],
       ),
-    );
-  }
-}
-
-class Stats extends StatelessWidget {
-  const Stats({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ValueLabel(
-              value: "\$1000",
-              label: "Money",
-            ),
-            ValueLabel(
-              value: "69",
-              label: "Sold",
-            ),
-            ValueLabel(
-              value: "420",
-              label: "In Stock",
-            ),
-          ],
-        ),
-        padding: EdgeInsets.all(20),
-      ),
-      // padding
-      margin: EdgeInsets.all(20),
-    );
-  }
-}
-
-class ValueLabel extends StatelessWidget {
-  final String value;
-  final String label;
-
-  const ValueLabel({super.key, required this.value, required this.label});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          value,
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-        ),
-        Text(
-          label,
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-        ),
-      ],
     );
   }
 }
